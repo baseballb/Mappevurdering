31476326667e52be23be7be4e4aea928921f30d2
Gabriel Kjølsrud Bøhn <gabriebo@stud.ntnu.no>
2023-12-10 17:21:50 +0100

    Implemented addDelayToTrain and getTrainDeparturesBasedOnDestination to TrainDispatchController.java

--- src/main/java/edu/ntnu/stud/TrainDispatchController.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchController.java
@@ -1,6 +1,7 @@
 package edu.ntnu.stud;
 
 import java.time.format.DateTimeParseException;
+import java.util.List;
 import java.util.Scanner;
 import java.time.LocalTime;
 
@@ -32,6 +33,8 @@ public class TrainDispatchController {
    * @param choice The user's choice as an integer.
    */
   public void processChoice(int choice) {
+    int trainId;
+    String destination;
     switch (choice) {
       case 1:
         System.out.println("Vis oversikt over togavganger");
@@ -44,7 +47,7 @@ public class TrainDispatchController {
           System.out.println("That's not a number! Try again:");
           scanner.next();
         }
-        int trainId = scanner.nextInt();
+        trainId = scanner.nextInt();
         scanner.nextLine(); // Consume the remaining newline
         System.out.println("Enter line: ");
         String line = scanner.nextLine();
@@ -65,7 +68,7 @@ public class TrainDispatchController {
           return;
         }
         System.out.println("Skriv inn destinasjon:");
-        String destination = scanner.nextLine();
+        destination = scanner.nextLine();
         if (destination == null || destination.isEmpty()) {
           System.out.println("Destinasjon kan ikke være null eller tom");
           return;
@@ -84,7 +87,7 @@ public class TrainDispatchController {
           System.out.println("Vennligst skriv inn et tall:");
           scanner.next(); // discard the non-integer input
         }
-        int trainIdToAssign = scanner.nextInt();
+        trainId = scanner.nextInt();
         scanner.nextLine(); // consume the remaining newline
         System.out.println("Enter track number: ");
         while (!scanner.hasNextInt()) {
@@ -94,7 +97,7 @@ public class TrainDispatchController {
         int trackNumber = scanner.nextInt();
         scanner.nextLine(); // consume the remaining newline
         try {
-          trainDispatchSystem.assignTrainToTrack(trainIdToAssign, trackNumber);
+          trainDispatchSystem.assignTrainToTrack(trainId, trackNumber);
           System.out.println("Togavgang tildelt spor!");
         } catch (IllegalArgumentException e) {
           System.out.println("Kunne ikke tildele spor: " + e.getMessage());
@@ -102,7 +105,31 @@ public class TrainDispatchController {
         break;
       case 4:
         System.out.println("Legg til forsinkelse på togavgang");
-        // Implement functionality for Option 4
+        System.out.println("Skriv inn tognummer: ");
+        while (!scanner.hasNextInt()) {
+          System.out.println("Vennligst skriv inn et tall:");
+          scanner.next(); // discard the non-integer input
+        }
+        trainId = scanner.nextInt();
+        scanner.nextLine(); // consume the remaining newline
+        System.out.println("Enter delay time (HH:MM): ");
+        String delayTime = scanner.nextLine();
+        if (delayTime == null || delayTime.isEmpty()) {
+          System.out.println("Delay time cannot be null or empty");
+          return;
+        }
+        try {
+          LocalTime.parse(delayTime);
+        } catch (DateTimeParseException e) {
+          System.out.println("Invalid delay time format. It should be HH:MM");
+          return;
+        }
+        try {
+          trainDispatchSystem.addDelayToTrain(trainId, delayTime);
+          System.out.println("Delay added successfully.");
+        } catch (IllegalArgumentException e) {
+          System.out.println("Error adding delay: " + e.getMessage());
+        }
         break;
       case 5:
         System.out.println("Søk etter togavgang (Tognummer)");
@@ -111,18 +138,31 @@ public class TrainDispatchController {
           System.out.println("Vennligst skriv inn et tall: ");
           scanner.next(); // discard the non-integer input
         }
-        int trainIdToSearch = scanner.nextInt();
+        trainId = scanner.nextInt();
         scanner.nextLine(); // consume the remaining newline
-        TrainDeparture trainDeparture = trainDispatchSystem.getTrainDepartureBasedOnID(trainIdToSearch);
+        TrainDeparture trainDeparture = trainDispatchSystem.getTrainDepartureBasedOnID(trainId);
         if (trainDeparture == null) {
-          System.out.println("Fant ingen togavgang med tognummer " + trainIdToSearch + ".");
+          System.out.println("Fant ingen togavgang med tognummer " + trainId + ".");
         } else {
           System.out.println(trainDeparture);
         }
         break;
       case 6:
-        System.out.println("You've selected Option 6");
-        // Implement functionality for Option 6
+        System.out.println("Search for train departures based on destination");
+        System.out.println("Enter destination: ");
+        destination = scanner.nextLine();
+        if (destination == null || destination.isEmpty()) {
+          System.out.println("Destination cannot be null or empty");
+          return;
+        }
+        List<TrainDeparture> departuresForDestination = trainDispatchSystem.getTrainDeparturesBasedOnDestination(destination);
+        if (departuresForDestination.isEmpty()) {
+          System.out.println("No train departures found for the provided destination");
+        } else {
+          for (TrainDeparture departure : departuresForDestination) {
+            System.out.println(departure);
+          }
+        }
         break;
       case 7:
         System.out.println("You've selected Option 7");
